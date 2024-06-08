9f498cb6a66919ee616610ba388ab7cea30e1181
Raphael Simon <simon.raphael@gmail.com>
2024-06-08 12:51:57 -0700

    Add connectivity info to list_devices example

--- examples/list_devices.py
+++ examples/list_devices.py
@@ -2,10 +2,10 @@ import os
 import sys
 import orjson
 from collections import defaultdict
-from typing import Dict
+from typing import Dict, List
 from pydantic import BaseModel
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from atlas import AtlasClient
+from atlas import AtlasClient, Device
 
 class PropertyValue(BaseModel):
     name: str
@@ -13,27 +13,14 @@ class PropertyValue(BaseModel):
     bias: str
     alias: str
 
-def list_devices(debug: bool = False) -> Dict[str, Dict[str, Dict[str, Dict[str, PropertyValue]]]]:
-    """
-    Return the device properties of all devices across all facilities indexed by
-    facility name, then by device kind, then by device name, then by property
-    name.
+class DeviceList(BaseModel):
+    by_id: Dict[str, Device]
+    by_kind: Dict[str, Dict[str, List[Device]]]
 
-    example:
-    {
-        "Facility 1": {
-            "Compressors": {
-                "Comp 1": {
-                    "SuctionPressure": {
-                        "name": "Suction Pressure",
-                        "kind": "analog",
-                        "bias": "output",
-                        "alias": "comp1_suction_pressure"
-                    }
-                }
-            }
-        }
-    }
+def list_devices(debug: bool = False) -> DeviceList:
+    """
+    Return the device across all facilities indexed by facility name, then by
+    device kind as well as a dictionary of all devices indexed by device ID.
     """
     client = AtlasClient(debug)
     try:
@@ -42,7 +29,8 @@ def list_devices(debug: bool = False) -> Dict[str, Dict[str, Dict[str, Dict[str,
         print(f"Error listing facilities: {e}")
         return {}
 
-    all_devices = {}
+    by_kind = {}
+    by_id = {}
     for facility in sorted(facilities, key=lambda facility: facility.display_name):
         if not facility.agents:
             continue
@@ -54,45 +42,33 @@ def list_devices(debug: bool = False) -> Dict[str, Dict[str, Dict[str, Dict[str,
             continue
         for device in sorted(devices, key=lambda device: device.name):
             device_map[device.kind].append(device)
+            by_id[device.id] = device
 
-        device_dict = {}
-        for kind, devices in device_map.items():
-            if len(devices) == 0:
-                continue
-            kind_dict = {}
-            for device in sorted(devices, key=lambda device: device.name):
-                properties_dict = {
-                    prop.key: PropertyValue(name=prop.value.name, kind=prop.value.kind, bias=prop.value.bias, alias=prop.value.alias)
-                    for prop in device.properties
-                }
-                if len(device.upstream) > 0:
-                    properties_dict["upstream"] = {conn.kind: conn.device_id for conn in device.upstream}
-                if len(device.downstream) > 0:
-                    properties_dict["downstream"] = {conn.kind: conn.device_id for conn in device.downstream}
-                if len(properties_dict) > 0:
-                    kind_dict[device.name] = properties_dict
-            if len(kind_dict) > 0:
-                device_dict[kind.title() + "s"] = kind_dict
+        by_kind[facility.display_name] = device_map
 
-        all_devices[facility.display_name] = device_dict
-
-    return all_devices
+    return DeviceList(by_id=by_id, by_kind=by_kind)
 
 if __name__ == "__main__":
     json_output = "--json" in sys.argv
     debug ="--debug" in sys.argv
 
-    devices = list_devices(debug)
+    device_list = list_devices(debug)
+    by_kind = device_list.by_kind
+    by_id = device_list.by_id
 
     if json_output:
-        print(orjson.dumps(devices, default=lambda x: x.model_dump() if isinstance(x, BaseModel) else x).decode("utf-8"))
+        print(orjson.dumps(by_kind, default=lambda x: x.model_dump() if isinstance(x, BaseModel) else x).decode("utf-8"))
         sys.exit(0)
 
-    for facility_name, facility in devices.items():
+    for facility_name, facility in by_kind.items():
         print(f"Facility: {facility_name}")
-        for kind, devices in facility.items():
+        for kind, by_kind in facility.items():
             print(f"  {kind}:")
-            for device_name, properties in devices.items():
-                print(f"    {device_name}")
-                for _, prop in properties.items():
-                    print(f"      {prop.name} ({prop.kind} {prop.bias}): {prop.alias}")
+            for device in by_kind:
+                print(f"    {device.name}")
+                for prop in device.properties:
+                    print(f"      {prop.value.name} ({prop.value.kind} {prop.value.bias}): {prop.value.alias}")
+                for up in device.upstream:
+                    print(f"      Upstream: {up.kind} to {by_id[up.device_id].name}")
+                for down in device.downstream:
+                    print(f"      Downstream: {down.kind} to {by_id[down.device_id].name}")
